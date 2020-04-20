fc156ef89f263c4c8399385056190546acb17a1e
theolamide <ajadidan@gmail.com>
2020-04-20 15:06:41 -0600

    Question defined for interview042020.py

--- Python/Interview042020.py
+++ Python/Interview042020.py
@@ -1,3 +1,5 @@
+# Given an object/dictionary with keys and values that consist of both strings and integers, design an algorithm to calculate and return the sum of all of the numeric values.
+# For example, given the following object/dictionary as input:
 A = {
     "cat": "bob",
     "dog": 23,
