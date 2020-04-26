eb121d258f2c88ca1970d95cb4d54d2ffb6889ff
theolamide <ajadidan@gmail.com>
2020-04-25 23:21:02 -0600

    Bit difference comparison question from leetcode added

--- Python/NumberGuess.py
+++ Python/NumberGuess.py
@@ -1,12 +1,15 @@
+import random
+
+
 def Number_guess(x):
     if x > 10:
         print(f"{x} is an invalid input")
     else:
-        b = [int(char) for char in str(x*9)]
-        print(f'Input: {x}, Output: {sum(b)+4}')
+        int_list = [int(char) for char in str(x*9)]
+        print(f'Input: {x}, Output: {sum(int_list)+4}')
 
 
-Number_guess(4)
-Number_guess(5)
-Number_guess(1)
-Number_guess(12)
+Number_guess(random.randint(1, 14))
+Number_guess(random.randint(1, 14))
+Number_guess(random.randint(1, 14))
+Number_guess(random.randint(1, 14))
