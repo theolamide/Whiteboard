1aa2b4f0da29ec2515ea2d290e34ec9992d64672
theolamide <ajadidan@gmail.com>
2020-05-18 22:58:09 -0400

    Removeduplicatesatonce and frequnecyqueries added

--- Python/removerDuplicatesAtOnce.py
+++ Python/removerDuplicatesAtOnce.py
@@ -1,11 +1,18 @@
 def removeDuplicates(A):
-    for i in range(0, len(A)-1):
-        L = len(A)
-        print(i, L)
-        if A[i] == A[i+1]:
-            del A[i+1]
-            # print(i, A[i], A)
-    print(A)
+    arrLength = len(A)
+    count = 0
+
+    while count < arrLength - 1:
+        Deleted = True
+        if A[count] == A[count+1] and Deleted:
+            del A[count+1]
+            arrLength -= 1
+            print("count", count, A)
+            print("length", arrLength)
+            Deleted = True
+        else:
+            count += 1
+            Deleted = False
 
 
 Arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
